dc11e11d6d088e57b3dd09ff5f5e0684864d12d7
FDR <delrincon@gmail.com>
2022-05-31 12:45:21 -0300

    lazyLoad + chunkRename

---
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom';
+
+
+
+const LazyLayout = () => {
+    return (
+        <div>
+            <h1>LazyLayout Page</h1>
+
+            <ul>
+                <li>
+                    <NavLink to="lazy1">Lazy 1</NavLink>
+                </li>
+                <li>
+                    <NavLink to="lazy2">Lazy 2</NavLink>
+                </li>
+                <li>
+                    <NavLink to="lazy3">Lazy 3</NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default LazyLayout
---
+++ src/01-lazyload/pages/NoLazy.tsx
@@ -0,0 +1,10 @@
+
+
+
+export const NoLazy = () => {
+    return (
+        <div>
+            NO LazyLoad Component
+        </div>
+    )
+}
